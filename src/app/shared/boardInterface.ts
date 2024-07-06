6a55901b28417b9a1bb49cfa0eb9467d5aa24916
otari nozadze <otikonozadze@gmail.com>
2024-07-06 17:46:26 +0400

    t

---
+++ src/app/shared/boardInterface.ts
@@ -0,0 +1,32 @@
+export interface Board {
+  boards: BoardElement[];
+}
+
+export interface BoardElement {
+  name: string;
+  columns: Column[];
+}
+
+export interface Column {
+  name: string;
+  tasks: Task[];
+}
+
+export interface Task {
+  title: string;
+  description: string;
+  status: Status;
+  subtasks: Subtask[];
+}
+
+export enum Status {
+  Doing = 'Doing',
+  Done = 'Done',
+  Empty = '',
+  Todo = 'Todo',
+}
+
+export interface Subtask {
+  title: string;
+  isCompleted: boolean;
+}
