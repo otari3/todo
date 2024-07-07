bcaf3c4213f75b276696b0352a824581683b5440
otari nozadze <otikonozadze@gmail.com>
2024-07-07 14:18:22 +0400

    finished board styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavForBoardsComponent } from './navForBoards/nav-for-boards/nav-for-boards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavForBoardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navForBoards/nav-for-boards/nav-for-boards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-for-boards',
+  templateUrl: './nav-for-boards.component.html',
+  styleUrl: './nav-for-boards.component.scss'
+})
+export class NavForBoardsComponent {
+
+}
